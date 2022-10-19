30678c759e6f3b032614bd5728b84a0388884c75
Ouzman <ouz001@csiro.au>
2022-10-19 11:53:41 +1030

    fence 4 is wrong

--- plots.r
+++ plots.r
@@ -15,8 +15,20 @@ path_output_files <- "W:/VF/Pinnaroo 2022/animal_log/jax_working_outputs/"
 pinnaroo_paddock_area <- st_read(paste0(path_output_files, "pinnaroo_paddock_area.shp"))
 pinnaroo_Vf_area <- st_read(paste0(path_output_files, "pinnaroo_Vf_area.shp"))                                
 
-VF_animals_logs <- read_csv(paste0(path_output_files,"animal_GPS_data_sf_trans_clip.csv"))
+VF_animals_logs <- read_csv(paste0(path_output_files,
+                                   "animal_GPS_data_clip_uncum_step2.csv"))
+                                   #"animal_GPS_data_sf_trans_clip.csv"))
 Control_animals_logs <- read_csv(paste0(path_output_files,"control_animal_GPS_data_sf_trans_clip.csv"))
+
+
+names(Control_animals_logs)
+names(VF_animals_logs)
+
+#make some dummy clms so I can join them
+Control_animals_logs <- Control_animals_logs %>% 
+  mutate(`Audio_values` = NA,
+         `Shock_values` = NA  )
+ 
 
 animal_logs <- rbind(VF_animals_logs,Control_animals_logs )
 
@@ -64,7 +76,28 @@ ggplot() +
 ### also need to find out where the north water point is.
 
 
-#### I would like to display the audio and pulse records as another colored dots
+#### I would like to display the audio and pulse records as another coloured dots
+str(animal_logs_sf)
 
-# audio_records_only <- VF_animals_logs_sf %>% 
-#   filter()
+
+audio_records_only <- animal_logs_sf %>% 
+   filter(Audio_values > 0)
+shock_records_only <- animal_logs_sf %>% 
+  filter(Shock_values > 0)
+
+ggplot() +
+  geom_sf(data = pinnaroo_paddock_area, color = "black", fill = NA) +
+  geom_sf(data = pinnaroo_Vf_area, color = "black", fill = NA) +
+  geom_sf(data = water_pts_sf ,color ="Blue") +
+  #geom_sf(data = audio_records_only ,color ="Red",alpha = 0.01) +
+  geom_sf(data = audio_records_only ,color ="Pink") +
+  geom_sf(data = animal_logs_sf ,alpha = 0.01) +
+  theme_bw()+
+  theme(#legend.position = "none",
+        axis.ticks = element_blank(), axis.text.x = element_blank(), axis.text.y = element_blank())+
+  facet_wrap(. ~ Jax_fence_ID)+
+  labs(
+    title = "Pinnaroo VF 2020 aniamal logs",
+    subtitle = "VF Fences are faceted. Balck dots are GPS pts and pink are GPS records with pulse cues only"
+    #y = "", x = ""
+    )
